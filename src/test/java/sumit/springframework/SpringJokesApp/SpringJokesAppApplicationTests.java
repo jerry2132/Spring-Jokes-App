da5ad2c4b6f86cfda68a1f254f329ed69aaebcaa
jerry2132 <sumitsinghrawat34@gmail.com>
2023-08-24 17:49:26 +0530

    commit applied

---
+++ src/test/java/sumit/springframework/SpringJokesApp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sumit.springframework.SpringJokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
